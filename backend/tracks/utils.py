2644d3f09383bd042c958a80611e5bb2b8b7ac62
Bryan Wong <bryanwong@Bryans-MacBook-Air.local>
2019-12-20 11:34:23 -0800

    Added new column data to topic_create and topic_edit

--- backend/tracks/utils.py
+++ backend/tracks/utils.py
@@ -6,10 +6,13 @@ from backend.prereqs.fetch import get_topics
 def create_track(form_data):
     track = Track(name=form_data["name"],
                   description=form_data["description"],
+                  focus=form_data["focus"],
+                  topic_num=form_data["topic_num"],
                   image=form_data["image"]
                   )
 
     track.topics = get_topics(form_data["topics"])
+    track.required_topics = get_topics(form_data["required_topics"])
 
     return track
 
@@ -20,5 +23,6 @@ def edit_track(track, form_data):
     track.description = form_data["description"]
     track.image = form_data["image"]
     track.topics = get_topics(form_data["topics"])
+    track.required_topics = get_topics(form_data["required_topics"])
 
     return
