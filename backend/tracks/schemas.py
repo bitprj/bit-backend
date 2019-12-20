7f3373253ef75811a0fa0c8fc73938b0c322f258
Bryan Wong <bryanwong@Bryans-MacBook-Air.local>
2019-12-20 11:33:34 -0800

    Updated Track model and schema

--- backend/tracks/schemas.py
+++ backend/tracks/schemas.py
@@ -6,13 +6,17 @@ from marshmallow import fields
 class TrackFormSchema(ma.Schema):
     name = fields.Str(required=True)
     description = fields.Str(required=True)
+    focus = fields.Str(required=True)
+    topic_num = fields.Int(required=True)
     image = fields.Str(required=True)
     # topics is a list of topic ids
     topics = fields.List(fields.Int(), required=False)
+    # required_topics is a list of topic ids. These are the topics that need to be completed by the user
+    required_topics = fields.List(fields.Int(), required=False)
 
     class Meta:
         # Fields to show when sending data
-        fields = ("name", "description", "image", "topics")
+        fields = ("name", "description", "focus", "topic_num", "image", "topics", "required_topics")
         ordered = True
 
 
@@ -21,14 +25,17 @@ class TrackSchema(ma.Schema):
     id = fields.Int(required=True)
     name = fields.Str(required=True)
     description = fields.Str(required=True)
+    focus = fields.Str(required=True)
+    topic_num = fields.Int(required=True)
     image = fields.Str(required=True)
     # We are referencing another Schema below. You do this in oder to avoid circular referencing
     # The only keyword is used to show the id of topics
     topics = fields.Nested("TopicSchema", only=("id",), many=True)
+    required_topics = fields.Nested("TopicSchema", only=("id",), many=True)
 
     class Meta:
         # Fields to show when sending data
-        fields = ("id", "name", "description", "image", "topics")
+        fields = ("id", "name", "description", "focus", "topic_num", "image", "topics", "required_topics")
         ordered = True
 
 
