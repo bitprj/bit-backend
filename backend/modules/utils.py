7bea63cf73ab28c5fbcc2e1780d3b71ba5ed839c
wongband <bmwong@ucdavis.edu>
2019-12-24 03:07:52 -0800

    Implemented FetchModuleProgress and UpdateModuleProgress

--- backend/modules/utils.py
+++ backend/modules/utils.py
@@ -1,4 +1,4 @@
-from backend.models import Module
+from backend.models import Activity, Module, Student
 from backend.prereqs.fetch import get_activities
 from backend.prereqs.utils import assign_badge_prereqs, delete_badge_prereqs
 
@@ -27,3 +27,28 @@ def edit_module(module, form_data):
     assign_badge_prereqs(form_data["badge_prereqs"], module, "Module")
 
     return
+
+
+# Function to return a student's current module progress based on the module id
+def get_module_progress(student_id, module_id):
+    student = Student.query.get(student_id)
+
+    activities = set(Activity.query.filter(Activity.modules.any(id=module_id)).all())
+    completed_activities = set(student.completed_activities).intersection(activities)
+    incomplete_activities = set(student.incomplete_activities).intersection(activities)
+
+    activity_progress = {"completed_activities": completed_activities,
+                         "incomplete_activities": incomplete_activities
+                         }
+
+    return activity_progress
+
+
+# Function to check if a module exists in the database
+def validate_module(module_id):
+    module = Module.query.get(module_id)
+
+    if not module:
+        return True
+
+    return False
