68004ee15bb6e7c263796ffd9caf53fb00514941
wongband <bmwong@ucdavis.edu>
2019-12-31 23:55:19 -0800

    Adjusted card routes to adjust for contentful

--- backend/activities/utils.py
+++ backend/activities/utils.py
@@ -1,4 +1,18 @@
-from backend.models import Activity
+from backend.models import Activity, Card
+
+
+# Function to add cards to activities
+def add_cards(activity, contentful_data):
+    cards = []
+
+    if contentful_data["cards"]:
+        for card in contentful_data["cards"]:
+            target_card = Card.query.filter_by(name=card.name).first()
+            cards.append(target_card)
+
+    activity.cards = cards
+
+    return
 
 
 # Function to create a activity
@@ -13,5 +27,6 @@ def create_activity(contentful_data):
 def edit_activity(contentful_data):
     activity = Activity.query.filter_by(contentful_id=contentful_data["entityId"]).first()
     activity.name = contentful_data["parameters"]["name"]["en-US"]
-
+    add_cards(activity, contentful_data)
+    
     return
