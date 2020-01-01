f89e35e7faef2575435077b34b6c12190fc0f3e4
wongband <bmwong@ucdavis.edu>
2019-12-31 16:10:50 -0800

    Refactored actvities

--- backend/activities/routes.py
+++ backend/activities/routes.py
@@ -1,40 +1,49 @@
 from flask import (Blueprint, request)
 from flask_restful import Resource
 from backend import api, db
-from backend.activities.utils import create_activity, get_activity
+from backend.activities.schemas import activities_schema
+from backend.activities.utils import create_activity, edit_activity, get_activity
 from backend.models import Activity
 
 # Blueprint for activities
 activities_bp = Blueprint("activities", __name__)
 
 
-# Class to Read and Destroy routes
-class ActivityData(Resource):
-    # Function to return data on a single activity
+# Class for activity CRUD routes
+class ActivityCRUD(Resource):
+    # Function to get all activities
     def get(self):
-        contentful_data = request.get_json()
-        activity = get_activity(contentful_data)
+        activities = Activity.query.all()
 
-        # If activity does not exists, then return a 404 error
-        # else return the activity back to the user
-        if not activity:
-            return {"message": "Activity does not exist"}, 404
-        else:
-            return {"contentful_id": activity.contentful_id}, 200
+        return activities_schema.dump(activities)
 
     # Function to create a activity
     def post(self):
         contentful_data = request.get_json()
         activity = create_activity(contentful_data)
+
         db.session.add(activity)
         db.session.commit()
 
-        return {"message": "Activity successfully created"}, 202
+        return {"message": "Activity successfully created"}, 201
 
-    # Function to delete a activity
+    # Function to edit an activity
     def put(self):
         contentful_data = request.get_json()
+        edit_activity(contentful_data)
+
+        db.session.commit()
+
+        return {"message": "Activity successfully updated"}, 200
+
+
+# This class is used to delete an activity with a POST request
+class ActivityDelete(Resource):
+    # Function to delete a activity!!
+    def post(self):
+        contentful_data = request.get_json()
         activity = Activity.query.filter_by(contentful_id=contentful_data["entityId"]).first()
+
         db.session.delete(activity)
         db.session.commit()
 
@@ -42,4 +51,6 @@ class ActivityData(Resource):
 
 
 # Creates the routes for the classes
-api.add_resource(ActivityData, "/activities")
+api.add_resource(ActivityCRUD, "/activities")
+api.add_resource(ActivityDelete, "/activities/delete")
+
--- backend/activities/utils.py
+++ backend/activities/utils.py
@@ -3,15 +3,23 @@ from backend.models import Activity
 
 # Function to create a activity
 def create_activity(contentful_data):
-    activity = Activity(contentful_id=contentful_data["sys"]["id"]
+    activity = Activity(contentful_id=contentful_data["entityId"]
                         )
 
     return activity
 
 
+# Function to edit an activity
+def edit_activity(contentful_data):
+    activity = Activity.query.filter_by(contentful_id=contentful_data["entityId"]).first()
+    activity.name = contentful_data["parameters"]["name"]["en-US"]
+
+    return
+
+
 # Function to get an activity based on its contentful id
 def get_activity(contentful_data):
-    contentful_id = contentful_data["sys"]["id"]
+    contentful_id = contentful_data["entityId"]
     activity = Activity.query.filter_by(contentful_id=contentful_id).first()
 
     return activity
