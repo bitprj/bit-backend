6ca70c6a387d8e87f6d1732f4a3d5a1d68e2da57
wongband <bmwong@ucdavis.edu>
2020-01-02 03:26:23 -0800

    Adjusted Hint routes for contentful

--- backend/hints/routes.py
+++ backend/hints/routes.py
@@ -1,97 +1,71 @@
 from flask import (Blueprint, request)
 from flask_restful import Resource
 from backend import api, db
-from backend.models import Card, Hint
-from backend.hints.schemas import hint_schema, hint_form_schema
+from backend.models import Hint
+from backend.hints.schemas import hint_schema
 from backend.hints.utils import create_hint, edit_hint
-from backend.steps.utils import validate_step_data
 
 # Blueprint for hints
 hints_bp = Blueprint("hints", __name__)
 
 
-# Class to Read, Update, and Destroy routes
-class HintData(Resource):
-    # Function to return data on a single hint
+# Class for hint CRUD routes
+class HintCRUD(Resource):
+    # Function to create a hint
+    def post(self):
+        contentful_data = request.get_json()
+        print(contentful_data)
+        hint = create_hint(contentful_data)
+
+        db.session.add(hint)
+        db.session.commit()
+        print(hint.id)
+        return {"message": "Hint successfully created"}, 201
+
+    # Function to edit an hint
+    def put(self):
+        contentful_data = request.get_json()
+        print(contentful_data)
+        hint = Hint.query.filter_by(contentful_id=contentful_data["entityId"]).first()
+        edit_hint(hint, contentful_data)
+
+        db.session.commit()
+        print(hint.id)
+
+        return {"message": "Hint successfully updated"}, 200
+
+
+# Function to get a specific Hint based on hint id
+class HintGetSpecific(Resource):
     def get(self, hint_id):
         hint = Hint.query.get(hint_id)
 
-        # If hint does not exists, then return a 404 error
-        # else return the hint back to the user
         if not hint:
             return {"message": "Hint does not exist"}, 404
-        else:
-            return hint_schema.dump(hint)
+        print(hint.id)
 
-    # Function to edit a hint
-    def put(self, hint_id):
-        hint = Hint.query.get(hint_id)
+        return hint_schema.dump(hint)
 
-        # If hint does not exist, then return a 404 error
-        # else edit a hint and edit it in the database
+
+# This class is used to delete an hint with a POST request
+class HintDelete(Resource):
+    # Function to delete a hint!!
+    def post(self):
+        contentful_data = request.get_json()
+        print(contentful_data)
+        hint = Hint.query.filter_by(contentful_id=contentful_data["entityId"]).first()
+
         if not hint:
             return {"message": "Hint does not exist"}, 404
-        else:
-            form_data = request.get_json()
-            errors = hint_form_schema.validate(form_data)
-            step_error = validate_step_data(form_data["steps"])
+        print(hint.id)
 
-            # If form data is not validated by the hint_schema, then return a 500 error
-            # else edit the hint and save it to the database
-            if errors or step_error:
-                return {
-                           "message": "Missing or sending incorrect data to edit a hint. Double check the JSON data that it has everything needed to edit a hint."
-                       }, 500
-            else:
-                edit_hint(hint, form_data)
-                db.session.commit()
-
-            return {"message": "Hint successfully updated"}, 202
-
-    # Function to delete a hint
-    def delete(self, hint_id):
-        hint = Hint.query.get(hint_id)
-
-        # If hint does not exists, return a 404 error
-        # else delete the hint and save to database
-        if not hint:
-            return {"message": "Hint does not exists"}, 404
-        else:
-            db.session.delete(hint)
-            db.session.commit()
+        db.session.delete(hint)
+        db.session.commit()
 
         return {"message": "Hint successfully deleted"}, 200
 
 
-# Class to define hint creation
-class HintCreate(Resource):
-    # Function to create a hint
-    def post(self, card_id):
-        form_data = request.get_json()
-        errors = hint_form_schema.validate(form_data)
-        step_error = validate_step_data(form_data["steps"])
-
-        # If form data is not validated by the hint_schema, then return a 500 error
-        # else create the hint and add it to the database
-        if errors or step_error:
-            return {
-                       "message": "Missing or sending incorrect data to create a hint. Double check the JSON data that it has everything needed to create a hint."
-                   }, 500
-        else:
-            card_error = Card.query.get(card_id)
-
-            if not card_error:
-                return {
-                           "message": "Card or Hint does not exist. Double check the card exists in the database."
-                       }, 500
-            else:
-                hint = create_hint(form_data, card_id)
-                db.session.add(hint)
-                db.session.commit()
-
-        return {"message": "Hint successfully created"}, 202
-
-
 # Creates the routes for the classes
-api.add_resource(HintData, "/hints/<int:hint_id>")
-api.add_resource(HintCreate, "/cards/<int:card_id>/hints/create")
+api.add_resource(HintCRUD, "/hints")
+api.add_resource(HintGetSpecific, "/hints/<int:hint_id>")
+api.add_resource(HintDelete, "/hints/delete")
