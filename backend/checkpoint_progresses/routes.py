96009fd32de566215b3da31f3f414dc05eeda66d
wongband <bmwong@ucdavis.edu>
2020-01-05 18:43:47 -0800

    Set up route to receive John's code

--- backend/checkpoint_progresses/routes.py
+++ backend/checkpoint_progresses/routes.py
@@ -3,7 +3,7 @@ from flask_praetorian.decorators import roles_accepted
 from flask_restful import Resource
 from backend import api, db
 from backend.general_utils import add_image, get_user_id_from_token
-from backend.models import CheckpointProgress
+from backend.models import Checkpoint, CheckpointProgress
 
 # Blueprint for checkpoints
 checkpoint_progresses_bp = Blueprint("checkpoint_progresses", __name__)
@@ -20,10 +20,15 @@ class CheckpointProgressSubmit(Resource):
         checkpoint_prog = CheckpointProgress.query.filter_by(checkpoint_id=checkpoint_id,
                                                              student_id=current_user_id).first()
         if checkpoint_prog:
-            image = add_image(image_file, "checkpoints")
-            checkpoint_prog.image_to_receive = image
-            checkpoint_prog.is_completed = True
+            checkpoint = Checkpoint.query.get(checkpoint_id)
 
+            if checkpoint.checkpoint_type == "Image":
+                image = add_image(image_file, "checkpoints")
+                checkpoint_prog.image_to_receive = image
+            elif checkpoint.checkpoint_type == "okPy":
+                print("data to receive from john's code")
+
+            checkpoint_prog.is_completed = True
             db.session.commit()
         else:
             return {
