5bc5c700f7b4559751f3b15e60acb2edbf18c7a2
wongband <bmwong@ucdavis.edu>
2020-01-16 19:28:44 -0800

    Added decorators for checkpoints

--- backend/checkpoints/routes.py
+++ backend/checkpoints/routes.py
@@ -1,6 +1,8 @@
 from flask import (Blueprint, request)
+from flask_jwt_extended import jwt_required
 from flask_restful import Resource
 from backend import api, db
+from backend.checkpoints.decorators import checkpoint_delete, checkpoint_exists, checkpoint_exists_in_contentful
 from backend.checkpoints.schemas import checkpoint_schema
 from backend.checkpoints.utils import create_checkpoint, edit_checkpoint
 from backend.models import Checkpoint
@@ -11,6 +13,8 @@ checkpoints_bp = Blueprint("checkpoints", __name__)
 
 # Class to Read, Create, and Update
 class CheckpointCRUD(Resource):
+    method_decorators = [checkpoint_exists_in_contentful]
+
     # Function to create a checkpoint
     def post(self):
         contentful_data = request.get_json()
@@ -25,10 +29,6 @@ class CheckpointCRUD(Resource):
     def put(self):
         contentful_data = request.get_json()
         checkpoint = Checkpoint.query.filter_by(contentful_id=contentful_data["entityId"]).first()
-
-        if not checkpoint:
-            return {"message": "Checkpoint does not exist"}, 404
-
         edit_checkpoint(checkpoint, contentful_data)
 
         db.session.commit()
@@ -38,13 +38,12 @@ class CheckpointCRUD(Resource):
 
 # This class is used to delete a checkpoint with a POST request
 class CheckpointDelete(Resource):
+    method_decorators = [checkpoint_delete]
+
     # Function to delete a checkpoint
     def post(self):
         contentful_data = request.get_json()
         checkpoint = Checkpoint.query.filter_by(contentful_id=contentful_data["entityId"]).first()
-
-        if not checkpoint:
-            return {"message": "Checkpoint does not exist"}, 404
 
         db.session.delete(checkpoint)
         db.session.commit()
@@ -54,16 +53,13 @@ class CheckpointDelete(Resource):
 
 # This class is used to get a specific checkpoint based on id
 class CheckpointGetSpecific(Resource):
+    method_decorators = [jwt_required, checkpoint_exists]
+
     # Function to return data on a single checkpoint
     def get(self, checkpoint_id):
         checkpoint = Checkpoint.query.get(checkpoint_id)
 
-        # If checkpoint does not exists, then return a 404 error
-        # else return the checkpoint back to the user
-        if not checkpoint:
-            return {"message": "Checkpoint does not exist"}, 404
-        else:
-            return checkpoint_schema.dump(checkpoint)
+        return checkpoint_schema.dump(checkpoint)
 
 
 # Creates the routes for the classes
