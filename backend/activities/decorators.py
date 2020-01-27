3c7f9f948fe6d15e0fda63d4521ddb8b0863b827
wongband <bmwong@ucdavis.edu>
2020-01-26 22:37:57 -0800

    Changed decorator for module progress

--- backend/activities/decorators.py
+++ backend/activities/decorators.py
@@ -1,7 +1,8 @@
 from flask import request
+from flask_jwt_extended import get_jwt_identity
 from backend import contentful_client
 from backend.config import SPACE_ID
-from backend.models import Activity
+from backend.models import Activity, Student
 from functools import wraps
 
 
@@ -38,6 +39,30 @@ def activity_exists_in_contentful(f):
                        "message": "Activity does not exist"
                    }, 404
 
+    return wrap
+
+
+# Decorator to check if a activity exists in contentful
+def activity_exists_in_student_prog(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        activity_completed = request.get_json()
+        username = get_jwt_identity()
+        student = Student.query.filter_by(username=username).first()
+        activity_id = activity_completed["complete"]["id"]
+        activity = Activity.query.get(activity_id)
+
+        if activity:
+            if activity in student.incomplete_activities:
+                return f(*args, **kwargs)
+            else:
+                return {
+                           "message": "Activity not in student's incomplete_activities"
+                       }, 500
+        else:
+            return {
+                       "message": "Activity does not exist"
+                   }, 500
     return wrap
 
 
