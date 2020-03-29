a36164bb3a736f40525b5802e0d48f03609f8e3a
wongband <bmwong@ucdavis.edu>
2020-03-29 15:09:40 -0700

    Adjusted FetchStudentData to take in query parameters for student_id

--- backend/students/schemas.py
+++ backend/students/schemas.py
@@ -10,16 +10,26 @@ class StudentSchema(ma.Schema):
     current_activities = fields.Nested("ActivitySchema", only=("id", "content_url"), many=True)
     inprogress_modules = fields.Nested("ModuleSchema", only=("id", "name"), many=True)
     inprogress_topics = fields.Nested("TopicSchema", only=("id", "name"), many=True)
-    current_topic = fields.Nested("TopicSchema", only=("id",), many=False)
-    current_track = fields.Nested("TrackSchema", only=("id",), many=False)
 
     class Meta:
         # Fields to show when sending data
         fields = (
-            "id", "name", "last_seen", "current_activities", "inprogress_modules", "inprogress_topics", "current_topic",
-            "current_track")
-        dateformat = '%Y-%m-%dT%H:%M:%S%z'
+            "id", "name", "last_seen", "current_activities", "inprogress_modules", "inprogress_topics")
+        ordered = True
+
+
+# This schema is used to display data based on the classroom that they are in
+class StudentClassroomSchema(ma.ModelSchema):
+    classes = fields.Nested("ClassroomSchema", only=("id", "modules"), many=True)
+    inprogress_modules = fields.Nested("ModuleSchema", only=("id",), many=True)
+    completed_activities = fields.Nested("ActivitySchema", only=("id",), many=True)
+    current_activities = fields.Nested("ActivitySchema", only=("id",), many=True)
+
+    class Meta:
+        # Fields to show when sending data
+        fields = ("classes", "inprogress_modules", "completed_activities")
         ordered = True
 
 
 student_schema = StudentSchema()
+student_classroom_schema = StudentClassroomSchema()
