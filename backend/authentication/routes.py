67d11c28fc72be96f43c27f000afaa72ce1898b9
wongband <bmwong@ucdavis.edu>
2020-04-15 02:03:57 -0700

    Fixed github sync error

--- backend/authentication/routes.py
+++ backend/authentication/routes.py
@@ -14,6 +14,7 @@ authentication_bp = Blueprint("authentication", __name__)
 @access_token_exists
 @github.authorized_handler
 def authorized(access_token):
+    existing_user = None
     oauth_user = None
     meta = None
 
@@ -23,17 +24,22 @@ def authorized(access_token):
     g.user = oauth_user
     github_user = github.get("/user")
     github_emails = github.get("/user/emails")
-    # existing_user = User.query.filter_by(github_id=github_user["id"]).first()
-    # TODO This is temporary, remove when all current users are synced with github
+    # TODO Temporary to get users to sync their account with github
     for email in github_emails:
-        existing_user = User.query.filter_by(email=email).first()
-        
+        existing_user = User.query.filter_by(username=email["email"]).first()
         if existing_user:
             break
+    # existing_user = User.query.filter_by(github_id=github_user["id"]).first()
 
     if existing_user:
+        # TODO Temporary to get users to sync their account with github
+        print(existing_user)
         oauth_user = existing_user
+        existing_user.github_id = github_user["id"]
+        existing_user.github_username = github_user["login"]
+        existing_user.github_access_token = access_token
         meta = oauth_user.meta
+        db.session.commit()
     else:
         meta = Meta(roles="Student")
         db.session.add(meta)
